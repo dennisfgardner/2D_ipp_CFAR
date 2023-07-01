d45465b21a1a04a730a74b4cf30a1188f13b5373
Dennis F Gardner <dennisfgardner@gmail.com>
2023-07-01 13:18:23 -0400

    initial working version

--- main.cpp
+++ main.cpp
@@ -5,61 +5,226 @@
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
 
+using std::cout, std::endl, std::string;
+
+#define DEBUG_PLOTS
+// #define VERBOSE
+
+#ifdef DEBUG_PLOTS 
+void DEBUG_PLOTS_helper(cv::Mat img, string description){
+    cv::Mat temp = img.clone();
+    temp.convertTo(temp, CV_32FC1);
+    // display the img in a window
+    cout << "examining " << description << endl;
+    double maxV, minV;
+    cv::minMaxIdx(temp, &minV, &maxV);
+    cout << "min " << minV << " maxV " << maxV << endl;
+    // rescale for DEBUG_PLOTSting (must be between 0 and 1)
+    temp = temp - minV;
+    temp = temp/maxV;
+    cv::namedWindow(description, cv::WINDOW_NORMAL);
+    cv::resizeWindow(description, 512, 512);
+    cv::imshow(description, temp);
+    int k = cv::waitKey(0);
+}
+#endif
+
+int calcSumBufferSize(cv::Mat img, IppiSize roiSize, IppiSize maskSize){
+    // return window sum buffer size
+    int bufferSize;
+    IppStatus ippSts = ippiSumWindowGetBufferSize(roiSize, maskSize, ipp32f, 1, &bufferSize);
+    #ifdef VERBOSE 
+    cout << ippGetStatusString(ippSts) << " when calculating the work butter size"  << "\n";
+    #endif
+    return bufferSize;
+}
+
+cv::Mat calcBoxSum(cv::Mat src, int winSize){
+    // return box sum
+    Ipp32u step = src.cols * sizeof(Ipp32f);
+    cv::Mat dst(cv::Size(src.cols, src.rows), CV_32F);
+    IppiSize roiSize = {src.cols, src.rows};
+    IppiSize maskSize = {winSize, winSize};
+    Ipp32f borderValue = 0;
+    int bufferSize = calcSumBufferSize(src, roiSize, maskSize);
+    Ipp8u *pBuffer = (Ipp8u*) ippMalloc(bufferSize);
+    IppStatus ippSts = ippiSumWindow_32f_C1R(
+        (const Ipp32f*)&src.data[0], step,
+        (Ipp32f*)&dst.data[0], step,
+        roiSize, maskSize,
+        ippBorderRepl, &borderValue,
+        pBuffer
+    );
+    #ifdef VERBOSE 
+    cout << ippGetStatusString(ippSts) << " when calculating the box sum"  << "\n";
+    #endif
+    ippFree(pBuffer);
+    return dst;
+}
+
+cv::Mat calcSubtraction(cv::Mat src1, cv::Mat src2){
+    // subtract src1 from src2, i.e. dst = src2 - src1
+    Ipp32u step = src1.cols * sizeof(Ipp32f);
+    cv::Mat dst(cv::Size(src1.cols, src1.rows), CV_32F);;
+    IppiSize roiSize = {src1.cols, src1.rows};
+    IppStatus ippSts = ippiSub_32f_C1R(
+        (const Ipp32f*)&src1.data[0], step, 
+        (const Ipp32f*)&src2.data[0], step,
+        (Ipp32f*)&dst.data[0], step,
+        roiSize
+    );
+    #ifdef VERBOSE 
+    cout << ippGetStatusString(ippSts) << " when calculating the difference"  << "\n";
+    #endif
+    return dst;
+}
+
+cv::Mat calcDivideScalar(cv::Mat src, int scalar){
+    Ipp32u step = src.cols * sizeof(Ipp32f);
+    cv::Mat dst(cv::Size(src.cols, src.rows), CV_32F);;
+    IppiSize roiSize = {src.cols, src.rows};
+    IppStatus ippSts = ippiDivC_32f_C1R(
+        (const Ipp32f*)&src.data[0], step,
+        scalar,
+        (Ipp32f*)&dst.data[0], step,
+        roiSize
+    );
+    #ifdef VERBOSE 
+    cout << ippGetStatusString(ippSts) << " when calculating the scalar division"  << "\n";
+    #endif
+    return dst;
+}
+
+cv::Mat calcDonutAvg(cv::Mat img, int innerSize, int outerSize){
+    cv::Mat outerSum = calcBoxSum(img, outerSize);
+    cv::Mat innerSum = calcBoxSum(img, innerSize);
+    cv::Mat donutSum = calcSubtraction(innerSum, outerSum);
+    int numPixels = outerSize*outerSize - innerSize*innerSize;
+    cv::Mat donutAvg = calcDivideScalar(donutSum, numPixels);
+    return donutAvg;
+}
+
+cv::Mat calcSquare(cv::Mat src){
+    Ipp32u step = src.cols * sizeof(Ipp32f);
+    cv::Mat dst(cv::Size(src.cols, src.rows), CV_32F);;
+    IppiSize roiSize = {src.cols, src.rows};
+    IppStatus ippSts = ippiSqr_32f_C1R(
+        (const Ipp32f*)&src.data[0], step,
+        (Ipp32f*)&dst.data[0], step,
+        roiSize
+    );
+    #ifdef VERBOSE 
+    cout << ippGetStatusString(ippSts) << " when calculating the square"  << "\n";
+    #endif
+    return dst;
+}
+
+cv::Mat calcSqrt(cv::Mat src){
+    Ipp32u step = src.cols * sizeof(Ipp32f);
+    cv::Mat dst(cv::Size(src.cols, src.rows), CV_32F);;
+    IppiSize roiSize = {src.cols, src.rows};
+    IppStatus ippSts = ippiSqrt_32f_C1R(
+        (const Ipp32f*)&src.data[0], step,
+        (Ipp32f*)&dst.data[0], step,
+        roiSize
+    );
+    #ifdef VERBOSE 
+    cout << ippGetStatusString(ippSts) << " when calculating the square root"  << "\n";
+    #endif
+    return dst;
+}
+
+cv::Mat calcMultiplyScalar(cv::Mat src, float scalar){
+    Ipp32u step = src.cols * sizeof(Ipp32f);
+    cv::Mat dst(cv::Size(src.cols, src.rows), CV_32F);;
+    IppiSize roiSize = {src.cols, src.rows};
+    IppStatus ippSts = ippiMulC_32f_C1R(
+        (const Ipp32f*)&src.data[0], step,
+        scalar,
+        (Ipp32f*)&dst.data[0], step,
+        roiSize
+    );
+    #ifdef VERBOSE 
+    cout << ippGetStatusString(ippSts) << " when calculating the scalar multiplication"  << "\n";
+    #endif
+    return dst;
+}
+
+cv::Mat calcAddition(cv::Mat src1, cv::Mat src2){
+    Ipp32u step = src1.cols * sizeof(Ipp32f);
+    cv::Mat dst(cv::Size(src1.cols, src1.rows), CV_32F);;
+    IppiSize roiSize = {src1.cols, src1.rows};
+    IppStatus ippSts = ippiAdd_32f_C1R(
+        (const Ipp32f*)&src1.data[0], step,
+        (const Ipp32f*)&src2.data[0], step,
+        (Ipp32f*)&dst.data[0], step,
+        roiSize
+    );
+    #ifdef VERBOSE 
+    cout << ippGetStatusString(ippSts) << " when calculating scalar addition"  << "\n";
+    #endif
+    return dst;
+}
+
+cv::Mat calcOutliers(cv::Mat src, cv::Mat threshold_values){
+    Ipp32u step_src = src.cols * sizeof(Ipp32f);
+    cv::Mat dst(cv::Size(src.cols, src.rows), CV_8UC1);
+    Ipp32u step_dst = src.cols * sizeof(Ipp8u);
+    IppiSize roiSize = {src.cols, src.rows};
+    IppStatus ippSts = ippiCompare_32f_C1R(
+        (const Ipp32f*)&src.data[0], step_src,
+        (const Ipp32f*)&threshold_values.data[0], step_src,
+        (Ipp8u*)&dst.data[0], step_dst,
+        roiSize,
+        ippCmpGreater
+    );
+    #ifdef VERBOSE 
+    cout << ippGetStatusString(ippSts) << " when calculating outliers"  << "\n";
+    #endif
+    return dst;
+}
+
 
 int main(){
-    std::cout << "Begin Main Program\n";
-
-    // parameters
-    Ipp8u winSize = 5;
+    cout << "Begin Main Program\n";
 
     // read image and display image
     cv::Mat img;
     img = cv::imread("hd-wallpaper-1867616.jpg", cv::IMREAD_GRAYSCALE);
-    std::string win_name = "Display Window";
-    cv::namedWindow(win_name, cv::WINDOW_NORMAL);
-    cv::resizeWindow(win_name, 512, 512);
-    cv::imshow(win_name, img);
-    int k = cv::waitKey(0);
+    img.convertTo(img, CV_32FC1);
 
-    // image properties
-    Ipp16u rows = img.rows;
-    Ipp16u cols = img.cols;
-    Ipp16u colors = img.channels();
-    std::cout << "rows " << rows << " cols " << cols << " channels "<< colors << "\n";
-    // I want to get the type of the MAT programatically
-    Ipp32u srcStep = rows * sizeof(Ipp8u);
-    Ipp32u total_num_bytes = rows * cols * colors * sizeof(Ipp8u) ;
-    std::cout << "The total number of bytes is " << total_num_bytes << "\n";
+    #ifdef DEBUG_PLOTS
+    DEBUG_PLOTS_helper(img, "input image");
+    #endif
 
-    // sum window function requires work buffer
-    IppiSize roiSize = {rows, cols};
-    IppiSize maskSize = {winSize, winSize};
-    IppDataType dataType = ipp32f;
-    int numChannels = 3;
-    int bufferSize = 0;
-    IppStatus ippSts = ippStsNoErr;
+    cv::Mat E_of_x = calcDonutAvg(img, 41, 101);
+    cv::Mat E_squared = calcSquare(E_of_x);
+    cv::Mat img_squared = calcSquare(img);
+    cv::Mat E_of_x_squared = calcDonutAvg(img_squared, 41, 101);
+    cv::Mat var = calcSubtraction(E_squared, E_of_x_squared);
+    cv::Mat std = calcSqrt(var);
 
-    ippSts = ippiSumWindowGetBufferSize(roiSize, maskSize, dataType, numChannels, &bufferSize);
-    std::cout << "ippSts: " << ippSts << "\n";
-    std::cout << "bufferSize: " << bufferSize << "\n";
-    Ipp8u *pBuffer = (Ipp8u*) ippMalloc(bufferSize);
-    // local background
-    Ipp32u dstStep = rows * sizeof(Ipp32s);
-    Ipp32s *pLocBkg = (Ipp32s*)ippMalloc(dstStep * rows);
-    Ipp8u borderValue = 0;
-    ippSts = ippiSumWindow_8u32s_C1R((const Ipp8u*)&img.data[0], srcStep, pLocBkg, dstStep, roiSize, maskSize, ippBorderRepl, &borderValue, pBuffer);
-    std::cout << "ippSts: " << ippSts << "\n";
+    #ifdef DEBUG_PLOTS
+    DEBUG_PLOTS_helper(E_of_x, "E[x]");
+    DEBUG_PLOTS_helper(E_squared, "E[x]^2");
+    DEBUG_PLOTS_helper(img_squared, "x^2");
+    DEBUG_PLOTS_helper(E_of_x_squared, "E[x^2]");
+    DEBUG_PLOTS_helper(var, "E[x^2] - E[x]^2");
+    DEBUG_PLOTS_helper(std, "std = sqrt{E[x^2] - E[x]^2}");
+    #endif
 
-    cv::Mat sumWindow(cv::Size(cols, rows), CV_8UC1, (uint8_t*)pLocBkg);
 
-    std::string win_name1 = "Display Window";
-    cv::namedWindow(win_name1, cv::WINDOW_NORMAL);
-    cv::resizeWindow(win_name1, 512, 512);
-    cv::imshow(win_name1, sumWindow);
-    int l = cv::waitKey(0);
+    // threshold to find pixels of interest
+    float deviation_from_std = 2.5;
+    cv::Mat deviations = calcMultiplyScalar(std, deviation_from_std);
+    cv::Mat threshold_values = calcAddition(E_of_x, deviations);
+    cv::Mat outliers = calcOutliers(img, threshold_values);
 
-    ippFree(pBuffer);
-    ippFree(pLocBkg);
+    #ifdef DEBUG_PLOTS
+    DEBUG_PLOTS_helper(threshold_values, "threshold values");
+    DEBUG_PLOTS_helper(outliers, "outliers");
+    #endif
 
     cv::destroyAllWindows();
+    return 0;
 }
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import argparse
 
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
+
+DEBUG = True
 
 
 def disp_img_cv(img, win_name="Display Window"):
@@ -25,9 +28,9 @@ def doughnut_mean(img, window_halfwidth, guard_halfwidth):
     win_width = 2*window_halfwidth + 1
     grd_width = 2*guard_halfwidth + 1
     win = cv2.boxFilter(img, cv2.CV_32F, (win_width, win_width),
-                        normalize=False)
+                        normalize=False, borderType=cv2.BORDER_REPLICATE)
     grd = cv2.boxFilter(img, cv2.CV_32F, (grd_width, grd_width),
-                        normalize=False)
+                        normalize=False, borderType=cv2.BORDER_REPLICATE)
     return (win - grd)/(win_width**2 - grd_width**2)
 
 
@@ -42,7 +45,7 @@ def cfar_2d(path_to_image):
     |           <-local hw->| local halfwidth
     |       __________      |
     |      |     <-g>|      | guard halfwidth
-    |      |    _    |      | 
+    |      |    _    |      |
     |      |   |_|   |      | pixel-under-test
     |      |   put   |      |
     |      |_________|      |
@@ -53,20 +56,28 @@ def cfar_2d(path_to_image):
     """
 
     img = cv2.imread(path_to_image, cv2.IMREAD_GRAYSCALE).astype(float)
-
+    if DEBUG:
+        print(f"img input min {np.min(img)} max {np.max(img)}")
     # calculate the standard deviation
     local_bkg_halfwidth = 50
     guard_halfwidth = 20
     # E[x], expectation value, i.e., the mean
     E_x = doughnut_mean(img, local_bkg_halfwidth, guard_halfwidth)
+    if DEBUG:
+        print(f"E_x min {np.min(E_x)} max {np.max(E_x)}")
     # E[x^2] expectation value of image values squared
     E_xsqrd = doughnut_mean(img**2, local_bkg_halfwidth, guard_halfwidth)
+    if DEBUG:
+        print(f"E_xsqrd min {np.min(E_xsqrd)} max {np.max(E_xsqrd)}")
     # standard deviation is sqrt(E[x^2] - (E[x])^2), see wiki for more details
     std_dev = cv2.sqrt(E_xsqrd - (E_x**2))
-
+    if DEBUG:
+        print(f"std_dev min {np.min(std_dev)} max {np.max(std_dev)}")
     # threshold to find pixels of interest
     deviation = 2.5
     threshold_values = E_x + deviation*std_dev
+    if DEBUG:
+        print(f"threshold_values min {np.min(threshold_values)} max {np.max(threshold_values)}")
     outliers = img > threshold_values
 
     # disp_img_mpl(img, "Original")
